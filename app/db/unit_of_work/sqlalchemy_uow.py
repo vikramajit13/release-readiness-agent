ab06ef11f946d8d3aca3df1da0419641c1888e8d
vikramajit13 <vikramajit13@gmail.com>
2026-03-27 19:04:02 +1100

    fix: update code for sqlALchecmy, UOW and idgenerators

---
+++ app/db/unit_of_work/sqlalchemy_uow.py
@@ -0,0 +1,28 @@
+from app.db.repositories.sql_document_repository import SqlDocumentRepository
+from app.db.repositories.sql_workflow_repository import SqlWorkflowRepository
+from app.db.session import AsyncSessionFactory
+
+
+class SqlAlchemyUnitOfWork:
+    def __init__(self, session_factory = AsyncSessionFactory):
+        self.session_factory = session_factory
+        self.session = None
+        self.documents = None
+        self.workflows = None
+
+    async def __aenter__(self):
+        self.session = self.session_factory()
+        self.documents = SqlDocumentRepository(self.session)
+        self.workflows = SqlWorkflowRepository(self.session)
+        return self
+
+    async def __aexit__(self, exc_type, exc, tb):
+        if exc:
+            await self.session.rollback()
+        await self.session.close()
+
+    async def commit(self):
+        await self.session.commit()
+
+    async def rollback(self):
+        await self.session.rollback()
